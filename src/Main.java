1e5b756a21531aa757451218c9383fa72cb21549
wuteebee <185331818+wuteebee@users.noreply.github.com>
2024-11-09 19:19:29 +0700

    menu created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Chay do an may dua oi");
+        Menu.MenuChinh();
     }
 }
---
+++ src/Menu.java
@@ -0,0 +1,203 @@
+
+import java.util.Scanner;
+
+public class Menu {
+
+    static Scanner input = new Scanner(System.in);
+
+    static void MenuChinh() {
+        int vongLap;
+        do {
+            vongLap = MenuChinh.xuatMenuChinh();
+            MenuChinh.xuLyMenuChinh(vongLap);
+        } while (vongLap != 0);
+        input.close();
+    }
+
+    static void clearScreen() {
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }
+}
+
+class MenuChinh {
+
+    static int xuatMenuChinh() {
+        int luaChon;
+        boolean dieuKien;
+        do {
+            System.out.println("0. Thoat MenuChinh.");
+            System.out.println("1. Nhan vien.");
+            System.out.println("2. Khach hang.");
+            System.out.println("3. Nha cung cap.");
+            luaChon = Menu.input.nextInt();
+            Menu.clearScreen();
+            // điều kiện vòng lặp
+            dieuKien = luaChon >= 0 && luaChon <= 3;
+            // nếu không thỏa điều kiện thì xuất thông báo
+            if (!dieuKien) {
+                System.out.println("Lua chon khong hop le, vui long chon lai!");
+            }
+        } while (!dieuKien); // nếu không thỏa thì cho nhập lại
+        return luaChon;
+    }
+
+    static void xuLyMenuChinh(int luaChon) {
+        int chon;
+        switch (luaChon) {
+            case 1:
+                do {
+                    chon = MenuNhanVien.xuatMenuNhanVien();
+                    MenuNhanVien.xuLyMenuNhanVien(chon);
+                } while (chon != 0);
+                break;
+            case 2:
+                do {
+                    chon = MenuKhachHang.xuatMenuKhachHang();
+                    MenuKhachHang.xuLyMenuKhachHang(chon);
+                } while (chon != 0);
+                break;
+            case 3:
+                do {
+                    chon = MenuNhaCungCap.xuatMenuNhaCungCap();
+                    MenuNhaCungCap.xuLyMenuNhaCungCap(chon);
+                } while (chon != 0);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+class MenuNhanVien {
+
+    static int xuatMenuNhanVien() {
+        int luaChon;
+        boolean dieuKien;
+        do {
+            System.out.println("0. Thoat MenuNhanVien.");
+            System.out.println("1. Them NhanVien.");
+            System.out.println("2. Chinh sua NhanVien.");
+            System.out.println("3. Danh sach NhanVien.");
+            System.out.println("4. Them Sach.");
+            System.out.println("5. Danh sach Sach.");
+            luaChon = Menu.input.nextInt();
+            Menu.clearScreen();
+            // điều kiện vòng lặp
+            dieuKien = luaChon >= 0 && luaChon <= 5;
+            // nếu không thỏa điều kiện thì xuất thông báo
+            if (!dieuKien) {
+                System.out.println("Lua chon khong hop le, vui long chon lai!");
+            }
+        } while (!dieuKien); // nếu không thỏa thì cho nhập lại
+        return luaChon;
+    }
+
+    static void xuLyMenuNhanVien(int luaChon) {
+        switch (luaChon) {
+            case 1:
+                System.out.println("Xu ly them NhanVien.");
+                break;
+            case 2:
+                System.out.println("Xu ly chinh sua NhanVien.");
+                break;
+            case 3:
+                System.out.println("Xu ly danh sach NhanVien.");
+                break;
+            case 4:
+                System.out.println("Xu ly them Sach.");
+                break;
+            case 5:
+                System.out.println("Xu ly danh sach Sach.");
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+class MenuKhachHang {
+
+    static int xuatMenuKhachHang() {
+        int luaChon;
+        boolean dieuKien;
+        do {
+            System.out.println("0. Thoat MenuKhachHang.");
+            System.out.println("1. Them KhachHang.");
+            System.out.println("2. Chinh sua KhachHang.");
+            System.out.println("3. Danh sach KhachHang.");
+            System.out.println("4. Muon/Tra/Phat");
+            luaChon = Menu.input.nextInt();
+            Menu.clearScreen();
+            // điều kiện vòng lặp
+            dieuKien = luaChon >= 0 && luaChon <= 4;
+            // nếu không thỏa điều kiện thì xuất thông báo
+            if (!dieuKien) {
+                System.out.println("Lua chon khong hop le, vui long chon lai!");
+            }
+        } while (!dieuKien); // nếu không thỏa thì cho nhập lại
+        return luaChon;
+    }
+
+    static void xuLyMenuKhachHang(int luaChon) {
+        switch (luaChon) {
+            case 1:
+                System.out.println("Xu ly them KhachHang.");
+                break;
+            case 2:
+                System.out.println("Xu ly chinh sua KhachHang.");
+                break;
+            case 3:
+                System.out.println("Xu ly danh sach KhachHang.");
+                break;
+            case 4:
+                System.out.println("Xu ly Muon/Tra/Phat.");
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+class MenuNhaCungCap {
+
+    static int xuatMenuNhaCungCap() {
+        int luaChon;
+        boolean dieuKien;
+        do {
+            System.out.println("0. Thoat MenuNhaCungCap.");
+            System.out.println("1. Them NhaCungCap.");
+            System.out.println("2. Chinh sua NhaCungCap.");
+            System.out.println("3. Danh sach NhaCungCap.");
+            System.out.println("4. Cung cap Sach.");
+            luaChon = Menu.input.nextInt();
+            Menu.clearScreen();
+            // điều kiện vòng lặp
+            dieuKien = luaChon >= 0 && luaChon <= 4;
+            // nếu không thỏa điều kiện thì xuất thông báo
+            if (!dieuKien) {
+                System.out.println("Lua chon khong hop le, vui long chon lai!");
+            }
+        } while (!dieuKien); // nếu không thỏa thì cho nhập lại
+        return luaChon;
+    }
+
+    static void xuLyMenuNhaCungCap(int luaChon) {
+        switch (luaChon) {
+            case 1:
+                System.out.println("Xu ly NhaCungCap.");
+                break;
+            case 2:
+                System.out.println("Xu ly chinh sua NhaCungCap.");
+                break;
+            case 3:
+                System.out.println("Xu ly danh sach NhaCungCap.");
+                break;
+            case 4:
+                System.out.println("Xu ly cung cap Sach.");
+                break;
+            default:
+                break;
+        }
+    }
+}
