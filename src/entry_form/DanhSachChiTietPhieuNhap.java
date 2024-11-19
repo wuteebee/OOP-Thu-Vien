5aef2bc910ce38df19e722e966e23f00fbd4bb5e
Minh Nhat Hua <sunlight21@icloud.com>
2024-11-19 13:43:45 +0700

    commit test

--- src/entry_form/DanhSachChiTietPhieuNhap.java
+++ src/entry_form/DanhSachChiTietPhieuNhap.java
@@ -4,6 +4,20 @@ public class DanhSachChiTietPhieuNhap {
     static int soLuong;
     private ChiTietPhieuNhap[] dSPN;
 
-
+     public void themChiTietPhieuNhap(ChiTietPhieuNhap chiTietPhieuNhap) {
+        if (dSPN == null) {
+            dSPN = new ChiTietPhieuNhap[1];
+            dSPN[0] = chiTietPhieuNhap;
+        } else {
+            ChiTietPhieuNhap[] newDSPN = new ChiTietPhieuNhap[dSPN.length + 1];
+            System.arraycopy(dSPN, 0, newDSPN, 0, dSPN.length);
+            newDSPN[dSPN.length] = chiTietPhieuNhap;
+            dSPN = newDSPN;
+        }
+        soLuong++;
+    }
 }
+
+
+
 
