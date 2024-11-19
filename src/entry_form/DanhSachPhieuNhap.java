14c52ca256ae753a88dc5c96c6e70ed8c11a9503
Minh Nhat Hua <sunlight21@icloud.com>
2024-11-19 14:08:45 +0700

    add+delete

--- src/entry_form/DanhSachPhieuNhap.java
+++ src/entry_form/DanhSachPhieuNhap.java
@@ -6,7 +6,24 @@ public class DanhSachPhieuNhap {
     static int soLuong;
     private DanhSachPhieuNhap[] dSPN;
 
+public void themPhieuNhap(DanhSachPhieuNhap phieuNhap) {
+        if (soLuong == dSPN.length) {//kiểm tra mảng đầy chưa
+            DanhSachPhieuNhap[] newDSPN = new DanhSachPhieuNhap[dSPN.length * 2];
+            System.arraycopy(dSPN, 0, newDSPN, 0, dSPN.length);
+            dSPN = newDSPN;
+        }
+        dSPN[soLuong++] = phieuNhap;
+    }
 
+    public void xoaPhieuNhap(int index) {
+        if (index < 0 || index >= soLuong) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        for (int i = index; i < soLuong - 1; i++) {
+            dSPN[i] = dSPN[i + 1];
+        }
+        dSPN[--soLuong] = null;
+    }
 
 
 }
