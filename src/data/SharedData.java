53cc2c9bce7007f0405c06ac74fe5c79245fc6b7
Thresk <141814547+Thresk@users.noreply.github.com>
2024-12-07 21:08:19 +0700

    Them cac danh sach tu package fine_ticket

--- src/data/SharedData.java
+++ src/data/SharedData.java
@@ -1,10 +1,15 @@
 package data;
 import book.*;
 import employee.*;
+import fine_ticket.*;
 
 public class SharedData {
     public static DanhSachSach dSS = new DanhSachSach();
     public static DanhSachTacGia dSTG = TestData.dataDSTG();
     public static DanhSachNhaXuatBan dSNXB = TestData.dataDSNXB();
     public static DanhSachNhanVien dSNV = TestData.dataDSNV();
+
+    public static DanhSachPhieuPhat dSPP = new DanhSachPhieuPhat();
+    public static DanhSachChiTietPhieuPhat dSCTPP = new DanhSachChiTietPhieuPhat();
+    public static DanhSachDieuKhoanViPham dSDK = new DanhSachDieuKhoanViPham();
 }
