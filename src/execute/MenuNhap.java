d43c6f738dfa5d73ef152f48f7eca8942fa343d6
Thresk <141814547+Thresk@users.noreply.github.com>
2024-12-10 00:07:27 +0700

    Update MenuNhap.java

--- src/execute/MenuNhap.java
+++ src/execute/MenuNhap.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 import data.*;
 
 public class MenuNhap {
-    public static void xuly(String[] args) {
+    public int xuly(String[] args) {
         int choice;
-        do {
+        Boolean dieuKien;
+        do
+        {
             System.out.println("0. Thoat MenuNhap");
             System.out.println("1. Tim kiem phieu nhap");
             System.out.println("2. Them phieu nhap");
@@ -15,7 +17,17 @@ public class MenuNhap {
             System.out.print("Nhap lua chon cua ban: ");
             choice = Menu.input.nextInt();
             Menu.input.nextLine(); 
+            Menu.clearScreen();
+            dieuKien = choice >= 0 && choice <= 5;
+            if (!dieuKien)
+            { 
+                System.out.println("Lua chon khong hop le, vui long nhap lai!");
+            }
+        } while (!dieuKien);
+        return choice;
+    }
 
+      public void xuLyMenuQuanLyKhachHang(int choice) {
             switch (choice) {
                 case 1:
                     danhSach.timPhieuNhap();
@@ -32,13 +44,8 @@ public class MenuNhap {
                 case 5:
                     danhSach.suaPhieuNhap();
                     break;
-                case 0:
-                    break;
                 default:
-                    System.out.println("Lua chon khong hop le. Vui long chon lai.");
+                    break;
             }
-        } while (choice != 0);
-
-        scanner.close();
     }
 }
